49b3436ec804b35fc0e769681e924033e3d4b41b
loocash <lukaszkapica@gmail.com>
2019-07-19 23:51:56 +0200

    Refactor Calculator.spec.js

--- src/spec/Calculator.spec.js
+++ src/spec/Calculator.spec.js
@@ -6,80 +6,81 @@ import Calculator from '../components/Calculator';
 
 afterEach(cleanup);
 
-let resistanceInput, toleranceInput, bandsInput, resistorSVG, resistorInfo, resistanceDiv, bandsDiv, eseriesDiv, rootContainer;
+let Elements = {};
 
 beforeEach(() => {
   const {container} = render(<Calculator />);
-  rootContainer = container;
 
-  resistanceInput = container.querySelector('.Resistance__resistance-input');
-  toleranceInput = container.querySelector('.Resistance__tolerance-input');
-  bandsInput = container.querySelector('.Resistance__bands-input');
-  resistorSVG = container.querySelector('.resistor-svg');
-  resistorInfo = container.querySelector('.Calculator__resistor-info');
-  resistanceDiv = container.querySelector('.Resistance');
-  bandsDiv = container.querySelector('.Bands');
-  eseriesDiv = container.querySelector('.ESeries');
+  Elements = _.mapValues({
+    resistanceInput: '.Resistance__resistance-input',
+    toleranceInput: '.Resistance__tolerance-input',
+    bandsInput: '.Resistance__bands-input',
+    resistorSVG: '.resistor-svg',
+    resistorInfo: '.Calculator__resistor-info',
+    bandsDiv: '.Bands',
+    eseriesDiv: '.ESeries',
+  }, queryString => container.querySelector(queryString));
+
+  Elements.container = container;
 });
 
 const helpers = {
-  getResistorSVGBands(resistorSVG) {
-    return Array.from(resistorSVG.querySelectorAll('[class^="Band--"]'))
+  getResistorSVGBands() {
+    return Array.from(Elements.resistorSVG.querySelectorAll('[class^="Band--"]'))
       .map(tag => tag.classList.value)
       .map(className => className.replace('Band--', ''))
       .map(className => _.capitalize(className));
   },
-  getResistanceInputValues(resistanceDiv) {
-    const resistanceInput = resistanceDiv.querySelector('.Resistance__resistance-input');
-    const toleranceInput = resistanceDiv.querySelector('.Resistance__tolerance-input');
-    const bandsInput = resistanceDiv.querySelector('.Resistance__bands-input');
-
-    return [resistanceInput.value, toleranceInput.value, bandsInput.value];
+  getResistanceInputValues() {
+    return [Elements.resistanceInput.value, Elements.toleranceInput.value, Elements.bandsInput.value];
   },
-  getSelectionBands(bandsDiv) {
-    const selects = bandsDiv.querySelectorAll('.ColorSelect__colors');
-    return Array.from(selects)
-      .map(select => select.value);
+  getSelectionBands() {
+    const selects = Elements.bandsDiv.querySelectorAll('.ColorSelect__colors');
+    return Array.from(selects).map(select => select.value);
   },
-  getESeries(eseriesDiv) {
-    return eseriesDiv.querySelector('.ESeries__content').innerHTML.split(', ');
+  getESeries() {
+    return Elements.eseriesDiv.querySelector('.ESeries__content').innerHTML.split(', ');
   },
-  getColorSelects(container) {
-    return container.querySelectorAll('.ColorSelect__colors');
+  getColorSelects() {
+    return Elements.container.querySelectorAll('.ColorSelect__colors');
   }
 };
 
 test('everything changes accordingly when the resistance fields are changed', () => {
+  const {resistanceInput, toleranceInput, bandsInput, resistorInfo} = Elements;
+
   fireEvent.change(resistanceInput, { target: { value: '150' } });
   fireEvent.change(toleranceInput, { target: { value: '2' } });
   fireEvent.change(bandsInput, { target: { value: '4' } });
 
   expect(resistorInfo.innerHTML).toBe("150Ω ± 2%");
-  expect(helpers.getResistorSVGBands(resistorSVG))
+  expect(helpers.getResistorSVGBands())
     .toEqual(['Brown', 'Green', 'Brown', 'Red']);
-  expect(helpers.getResistanceInputValues(resistanceDiv))
+  expect(helpers.getResistanceInputValues())
     .toEqual(['150', '2', '4']);
-  expect(helpers.getSelectionBands(bandsDiv))
+  expect(helpers.getSelectionBands())
     .toEqual(['Brown', 'Green', 'Brown', 'Red']);
-  expect(helpers.getESeries(eseriesDiv))
+  expect(helpers.getESeries())
     .toEqual(['E6', 'E12', 'E24', 'E96']);
 });
 
 test('everything changes accordingly when color bands are changed', () => {
+  const {bandsInput, resistorInfo} = Elements;
+
   fireEvent.change(bandsInput, { target: { value: '4' } });
-  const colorSelects = helpers.getColorSelects(rootContainer);
+  const colorSelects = helpers.getColorSelects();
   fireEvent.change(colorSelects[0], { target: { value: 'Brown' } });
   fireEvent.change(colorSelects[1], { target: { value: 'Green' } });
   fireEvent.change(colorSelects[2], { target: { value: 'Brown' } });
   fireEvent.change(colorSelects[3], { target: { value: 'Red' } });
 
   expect(resistorInfo.innerHTML).toBe("150Ω ± 2%");
-  expect(helpers.getResistorSVGBands(resistorSVG))
+  expect(helpers.getResistorSVGBands())
     .toEqual(['Brown', 'Green', 'Brown', 'Red']);
-  expect(helpers.getResistanceInputValues(resistanceDiv))
+  expect(helpers.getResistanceInputValues())
     .toEqual(['150', '2', '4']);
-  expect(helpers.getSelectionBands(bandsDiv))
+  expect(helpers.getSelectionBands())
     .toEqual(['Brown', 'Green', 'Brown', 'Red']);
-  expect(helpers.getESeries(eseriesDiv))
+  expect(helpers.getESeries())
     .toEqual(['E6', 'E12', 'E24', 'E96']);
 });
