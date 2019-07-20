a5aaa964f46f3de9cd0481987ee6cb627b61673a
loocash <lukaszkapica@gmail.com>
2019-07-20 21:11:11 +0200

    Migrate Bands.spec.js to react-testing-library

--- src/spec/Bands.spec.js
+++ src/spec/Bands.spec.js
@@ -1,100 +1,31 @@
 import React from 'react';
-import Bands, {ColorSelect, pluckSecond} from '../components/Bands';
-import {valueAndColor, multiplierAndColor, toleranceAndColor} from '../lib/ColorCode';
+import Bands from '../components/Bands';
 
-import {shallow, configure} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import {render, cleanup, fireEvent} from "@testing-library/react";
 
-configure({adapter: new Adapter()});
+afterEach(cleanup);
 
-describe('<Bands />', () => {
+let rootContainer, props;
 
-  let wrapper, props;
-
-  const sampleProps = () => ({
+beforeEach(() => {
+  props = {
     onCodeChange: jest.fn(),
     code: ["Brown", "Orange", "Black", "Black", "Gold"],
-  });
-
-  beforeEach(() => {
-    props = sampleProps();
-    wrapper = shallow(<Bands {...props} />);
-  });
-
-  it('renders ColorSelect for every color in the code array', () => {
-    const renderedCode = wrapper.find('ColorSelect').map(tag => tag.props().value);
-    expect(renderedCode).toEqual(props.code);
-  });
-
-  it('renders ColorSelects for digits', () => {
-    const numberOfDigits = props.code.length - 2;
-    const digitSelects = wrapper.find('ColorSelect').slice(0, numberOfDigits);
-    const digitColors = pluckSecond(valueAndColor);
-    digitSelects.forEach(digitSelect => {
-      const colors = digitSelect.props().colors;
-      expect(colors).toEqual(digitColors);
-    });
-  });
-
-  it('renders ColorSelect for multiplier', () => {
-    const multiplierIndex = props.code.length - 2;
-    const multiplierSelect = wrapper.find('ColorSelect').get(multiplierIndex);
-    const multiplierColors = pluckSecond(multiplierAndColor);
-    const colors = multiplierSelect.props.colors;
-    expect(colors).toEqual(multiplierColors);
-  });
-
-  it('renders ColorSelect for tolerance', () => {
-    const toleranceIndex = props.code.length - 1;
-    const toleranceSelect = wrapper.find('ColorSelect').get(toleranceIndex);
-    const toleranceColors = pluckSecond(toleranceAndColor);
-    const colors = toleranceSelect.props.colors;
-    expect(colors).toEqual(toleranceColors);
-  });
-
-  it('calls onCodeChange when the code is changed', () => {
-    const firstDigit = wrapper.find('ColorSelect').get(0);
-    firstDigit.props.onColorChange("Brown"); // so nothing really changes
-    expect(props.onCodeChange.mock.calls.length).toBe(0);
-    firstDigit.props.onColorChange("Red");
-    expect(props.onCodeChange).toBeCalledWith(["Red", ...props.code.slice(1)]);
-  });
-
+  };
+  const {container} = render(<Bands {...props} />);
+  rootContainer = container;
 });
 
-describe('<ColorSelect />', () => {
+test('renders ColorSelect for every color in the code array', () => {
+  const renderedCode = Array.from(rootContainer.querySelectorAll('.ColorSelect__colors'))
+    .map(tag => tag.value);
+  expect(renderedCode).toEqual(props.code);
+});
 
-  let wrapper, props;
-
-  const sampleProps = () => ({
-    value: "Red",
-    onColorChange: jest.fn(),
-    title: "Band 1",
-    colors: ["Red", "Black", "Orange"],
-  });
-
-  beforeEach(() => {
-    props = sampleProps();
-    wrapper = shallow(<ColorSelect {...props} />);
-  });
-
-  it('renders a title', () => {
-    const actual = wrapper.find('.ColorSelect__title').text();
-    expect(actual).toBe(props.title);
-  });
-
-  it('renders <select> tag with all given colors as options and proper value selected', () => {
-    const actual = wrapper.find('option').map(tag => tag.text());
-    expect(actual).toEqual(props.colors);
-    expect(wrapper.find('select').props().value).toBe(props.value);
-  });
-
-  it('calls onColorChange when new color is selected', () => {
-    const select = wrapper.find('select');
-    select.simulate('change', {target:{ value: "Red" }}); // Simulate choosing the same color
-    expect(props.onColorChange.mock.calls.length).toBe(0);
-    select.simulate('change', {target:{ value: "Black" }}); // Simulate choosing different color
-    expect(props.onColorChange).toBeCalledWith("Black");
-  });
-
+test('calls onCodeChange when the code is changed', () => {
+  const firstDigit = rootContainer.querySelector('.ColorSelect__colors');
+  fireEvent.change(firstDigit, { target: { value: "Brown" } });
+  expect(props.onCodeChange).toHaveBeenCalledTimes(0);
+  fireEvent.change(firstDigit, { target: { value: "Red" } });
+  expect(props.onCodeChange).toBeCalledWith(["Red", ...props.code.slice(1)]);
 });
