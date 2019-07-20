5292aa710768cf68b436c3494e483aa6218d0cb6
loocash <lukaszkapica@gmail.com>
2019-07-20 22:21:02 +0200

    Migrate Resistance.spec.js to react-testing-library. Remove enzyme from dependencies

--- src/spec/Resistance.spec.js
+++ src/spec/Resistance.spec.js
@@ -1,63 +1,47 @@
 import React from 'react';
 
-import {shallow, configure} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
 import Resistance from '../components/Resistance';
 import { Resistor } from '../lib/ColorCode';
 
-configure({adapter: new Adapter()});
+import {render, cleanup, fireEvent} from "@testing-library/react";
 
-describe('<Resistance />', () => {
+afterEach(cleanup);
 
-  let wrapper, props;
+let rootContainer, props;
 
-  const sampleProps = () => ({
-    onBandsChange: jest.fn(),
-    onResistanceChange: jest.fn(),
-    onToleranceChange: jest.fn(),
-    resistor: new Resistor(130, 5, 5),
-  });
+const sampleProps = () => ({
+  onBandsChange: jest.fn(),
+  onResistanceChange: jest.fn(),
+  onToleranceChange: jest.fn(),
+  resistor: new Resistor(130, 5, 5),
+});
 
-  beforeEach(() => {
-    props = sampleProps();
-    wrapper = shallow(<Resistance {...props} />);
-  });
+beforeEach(() => {
+  props = sampleProps();
+  const {container} = render(<Resistance {...props} />);
+  rootContainer = container;
+});
 
-  it('renders an input for resistance, tolerance and number of bands', () => {
-    const selectors = [
-      '.Resistance__resistance-input',
-      '.Resistance__tolerance-input',
-      '.Resistance__bands-input',
-    ];
+test('calls onResistanceChange when the resistance is changed', () => {
+  const resistanceInput = rootContainer.querySelector('.Resistance__resistance-input');
+  fireEvent.change(resistanceInput, { target: { value: props.resistor.resistance }}); // nothing changes
+  expect(props.onResistanceChange).toHaveBeenCalledTimes(0);
+  fireEvent.change(resistanceInput, { target: { value: 100 }}); // now we have a change
+  expect(props.onResistanceChange).toHaveBeenCalledWith(100);
+});
 
-    selectors.forEach(selector =>
-      expect(wrapper.find(selector).exists()).toBe(true)
-    );
-  });
+test('calls onToleranceChange when the tolerance is changed', () => {
+  const toleranceInput = rootContainer.querySelector('.Resistance__tolerance-input');
+  fireEvent.change(toleranceInput, { target: { value: props.resistor.tolerance }}); // nothing changes
+  expect(props.onToleranceChange).toHaveBeenCalledTimes(0);
+  fireEvent.change(toleranceInput, { target: { value: 10 }});
+  expect(props.onToleranceChange).toHaveBeenCalledWith(10);
+});
 
-  it('calls onResistanceChange when the resistance is changed', () => {
-    const resistanceInput = wrapper.find('.Resistance__resistance-input').get(0);
-    resistanceInput.props.onResistanceChange(props.resistor.resistance); // nothing changes
-    expect(props.onResistanceChange.mock.calls.length).toBe(0);
-    resistanceInput.props.onResistanceChange(100);
-    expect(props.onResistanceChange).toBeCalledWith(100);
-  });
-
-  it('calls onToleranceChange when the tolerance is changed', () => {
-    const toleranceInput = wrapper.find('.Resistance__tolerance-input').get(0);
-    toleranceInput.props.onToleranceChange(props.resistor.tolerance); // nothing changes
-    expect(props.onToleranceChange.mock.calls.length).toBe(0);
-    toleranceInput.props.onToleranceChange(10);
-    expect(props.onToleranceChange).toBeCalledWith(10);
-  });
-
-  it('calls onBandsChange when the number of bands is changed', () => {
-    const bandsInput = wrapper.find('.Resistance__bands-input').get(0);
-    bandsInput.props.onBandsChange(props.resistor.bands); // nothing changes
-    expect(props.onBandsChange.mock.calls.length).toBe(0);
-    bandsInput.props.onBandsChange(4);
-    expect(props.onBandsChange).toBeCalledWith(4);
-  });
-
+test('calls onBandsChange when the number of bands is changed', () => {
+  const bandsInput = rootContainer.querySelector('.Resistance__bands-input');
+  fireEvent.change(bandsInput, { target: { value: props.resistor.bands }}); // nothing changes
+  expect(props.onBandsChange).toHaveBeenCalledTimes(0);
+  fireEvent.change(bandsInput, { target: { value: 4 }});
+  expect(props.onBandsChange).toHaveBeenCalledWith(4);
 });
