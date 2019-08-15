24c33ea7a7b1d8870eb3edc23f8478a6b5cabb8d
loocash <lukaszkapica@gmail.com>
2019-08-15 20:22:37 +0200

    refactor NBar

--- src/components/NBar.js
+++ src/components/NBar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -9,10 +11,12 @@ const ResistorIcon = () =>
     className="resistor-icon"
     src={`${process.env.PUBLIC_URL}/resistor-icon.svg`} />;
 
+/*
 const GithubIcon = () =>
   <ReactSVG
     className="github-icon"
     src={`${process.env.PUBLIC_URL}/github-brands.svg`} />;
+*/
 
 export default function NBar({
   bands,
@@ -32,7 +36,7 @@ export default function NBar({
             activeKey={bands}
             onSelect={(selectedKey, e) => {
               e.preventDefault();
-              onBandsChange(selectedKey);
+              onBandsChange(+selectedKey);
             }}
           >
             <Nav.Link href="3">3 bands</Nav.Link>
@@ -40,18 +44,23 @@ export default function NBar({
             <Nav.Link href="5">5 bands</Nav.Link>
           </Nav>
         </Navbar.Collapse>
-        <Navbar.Collapse className="justify-content-end">
-          <Nav className="ml-auto">
-            {/*<Nav.Item>
-                <NavDropdown title="Language" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#polish">Polski</NavDropdown.Item>
-                  <NavDropdown.Item href="#english">English</NavDropdown.Item>
-                </NavDropdown>
-              </Nav.Item>*/}
-            <Nav.Link active={false} target="_blank" rel="noopener noreferrer" href="https://github.com/loocash/resistor-calc"><GithubIcon /></Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        {/*<Navbar.Collapse className="justify-content-end">
+            <Nav className="ml-auto">
+              <Nav.Item>
+                  <NavDropdown title="Language" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#polish">Polski</NavDropdown.Item>
+                    <NavDropdown.Item href="#english">English</NavDropdown.Item>
+                  </NavDropdown>
+                </Nav.Item>
+              <Nav.Link active={false} target="_blank" rel="noopener noreferrer" href="https://github.com/loocash/resistor-calc"><GithubIcon /></Nav.Link>
+            </Nav>
+          </Navbar.Collapse>*/}
       </Container>
     </Navbar>
   );
 }
+
+NBar.propTypes = {
+  bands: PropTypes.number.isRequired,
+  onBandsChange: PropTypes.func.isRequired,
+};
